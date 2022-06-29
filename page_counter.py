836de2a8ba55172b33eb42a0c3652b0385ce091a
Justin Wozniak <woz@anl.gov>
2022-06-29 16:46:39 -0500

    Pretty much done

--- page_counter.py
+++ page_counter.py
@@ -14,22 +14,25 @@ def parse_args():
                         type=lambda s: dt.datetime.strptime(s, '%Y-%m-%d'),
                         help="Date to start.  Default: today")
     args = parser.parse_args()
-
-
     return args
 
-def validate(args):
-    pass
+
+def abort(msg):
+    print("error: " + msg)
+    exit(1)
+
 
 def main():
     args = parse_args()
-    print(str(args))
     span = args.deadline - args.start
+    if span.days < 0: abort("time span is negative!")
     print("days to go: %3i" % span.days)
     pages = args.total - args.current
+    if pages < 0: abort("pages remaining is negative!")
     print("pages left: %3i" % pages)
     rate = pages / span.days
     print("pages/day:  %5.1f" % rate)
+    print("")
 
     import random
     limit = 5
@@ -38,9 +41,8 @@ def main():
         day = args.start + td
         print(day.strftime("%Y-%m-%d "), end='')
         page = args.current + rate * i
-        print("page:  %4.1f" % page)
         ipage = int(page) + (random.random() < page - int(page))
-        print("ipage: %4.1f" % ipage)
+        print("%4i" % ipage)
 
 if __name__ == "__main__":
     main()
